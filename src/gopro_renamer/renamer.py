192c1710644eb7533e699085dc41b5f972887971
Kevin Ha <kcha@users.noreply.github.com>
2022-02-09 23:43:42 -0500

    Keep only start year in copyright

--- src/gopro_renamer/renamer.py
+++ src/gopro_renamer/renamer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2014,2018 Kevin Ha
+# Copyright (c) 2014 Kevin Ha
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
